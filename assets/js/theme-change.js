d3e682fba124bb0f9450431be1b13536b36a0055
Rakesh Vajrapu <rakeshrb1411@gmail.com>
2023-12-10 12:19:26 +0530

    Fixed Javscript Code

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -13,9 +13,9 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
-    }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }    
 }
 
